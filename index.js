1dd9d8d1140a9233be3be05295e1fe96e839e79b
Marcin Bonk <marvcin@gmail.com>
2022-02-03 22:56:51 +0100

    Updated to request handling

--- index.js
+++ index.js
@@ -67,9 +67,19 @@ try {
         json: data
     };
 
-    const request = https.request(options, () => {
-        console.log('App deployed');
-        request.end();
+    const request = https.request(options, (response) => {
+        response.on('data', function (chunk) {
+            console.log('Got response')
+        });
+
+        response.on('end', function () {
+            if (response.statusCode > 299 || response.statusCode < 200) {
+                console.log("Api call failed with response code " + response.statusCode);
+            } else {
+                console.log('App deployed');
+            }
+            request.end();
+        });
     });
 
     request.on('error', error => core.setFailed(error.message))
