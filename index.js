3703d78aa749d652db4496428addcb9aae751ac8
Marcin Bonk <marvcin@gmail.com>
2022-02-03 18:12:18 +0100

    Init

---
+++ index.js
@@ -0,0 +1,40 @@
+const core = require('@actions/core');
+const https = require('https');
+
+try {
+    const data = {
+        "charts": [
+            {
+                "chartName": core.getInput('chartName'),
+                "releaseName": core.getInput('releaseName'),
+                "annotations": core.getInput('chartAnnotations') || {},
+                "values": core.getInput('values') || {}
+            }
+        ],
+        "noHooks": false,
+        "timeout": "600s",
+        "wait": true,
+        "namespace": core.getInput('appNamespace'),
+        "projectId": `${core.getInput('clusterId')}/${core.getInput('projectId')}`,
+        "disableOpenAPIValidation": false,
+        "skipCRDs": false
+    };
+
+    const options = {
+        hostname: core.getInput('rancherUrl'),
+        port: 443,
+        path: `/v1/catalog.cattle.io.clusterrepos/${core.getInput('chartNamespace')}?action=install`,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${core.getInput('rancherToken')}`
+        },
+        json: data
+    };
+
+    https
+        .request(options)
+        .on('error', error => core.setFailed(error.message));
+} catch (error) {
+    core.setFailed(error.message)
+}
